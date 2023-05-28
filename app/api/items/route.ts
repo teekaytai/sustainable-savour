541dff9327c72f615905f8c5574b06f6a6661db8
teekaytai <90845551+teekaytai@users.noreply.github.com>
2023-05-29 00:50:20 +0800

    Refactor items route

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -1,13 +1,10 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { supabase } from '../../../lib/supabaseClient';
 
-export async function GET(request: Request) { 
-  let { data: items } = await supabase.from('items').select('*')
-  
-  return new Response(JSON.stringify(items), {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    status: 200,
-    statusText: 'OK'
-  })
+export async function GET(request: NextRequest) { 
+  let { data: items } = await supabase
+    .from('items')
+    .select('*')
+
+  return NextResponse.json(items)
 }

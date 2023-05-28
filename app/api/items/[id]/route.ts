7b4d050e38d8331f723df704da82e452ab2c6cd6
teekaytai <90845551+teekaytai@users.noreply.github.com>
2023-05-29 01:09:34 +0800

    Add error handling to API

--- app/api/items/[id]/route.ts
+++ app/api/items/[id]/route.ts
@@ -3,16 +3,21 @@ import { supabase } from '../../../../lib/supabaseClient';
 
 type Props = {
   params: Record<string, string>
-}
+};
 
 export async function GET(request: NextRequest, { params }: Props) { 
-  let { data: items } = await supabase
+  let { data: items, error } = await supabase
     .from('items')
     .select('*')
     .eq('id', params.id);
   
+  if (error) {
+    console.error('Error fetching data:', error.message);
+    return NextResponse.json({ error: 'Error fetching data' }, { status: 500 });
+  }
+  
   if (items?.length) {
-    return NextResponse.json(items[0])
+    return NextResponse.json(items[0]);
   } else {
     return NextResponse.json({ error: 'Resource not found' }, { status: 404 });
   }
--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -2,9 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import { supabase } from '../../../lib/supabaseClient';
 
 export async function GET(request: NextRequest) { 
-  let { data: items } = await supabase
+  let { data: items, error } = await supabase
     .from('items')
-    .select('*')
+    .select('*');
 
-  return NextResponse.json(items)
+  if (error) {
+    console.error('Error fetching data:', error.message);
+    return NextResponse.json({ error: 'Error fetching data' }, { status: 500 });
+  }
+  return NextResponse.json(items);
 }

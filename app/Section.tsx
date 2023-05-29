3ac0888a20f18ec8889721c3c4dacb727740c2e9
xlzior <wenjun.lye@gmail.com>
2023-05-29 23:07:26 +0800

    Change home page to client component

--- app/Section.tsx
+++ app/Section.tsx
@@ -1,15 +1,29 @@
 "use client"
 
-import { Heading, Stack } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import Carousel from "./Carousel";
 import { Item } from "./types";
+import { useEffect, useState } from "react";
 
 type SectionProps = {
   title: string;
-  items: Item[];
 }
 
-export default function Section({ title, items }: SectionProps) {
+export default function Section({ title }: SectionProps) {
+  const [items, setItems] = useState([])
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const res = await fetch('/api/items')
+        const data = await res.json()
+        setItems(data)
+      } catch {
+
+      }
+    }
+    getItems()
+  }, [])
+
   return (
     <>
       <Heading size="lg">{title}</Heading>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import { headers } from "next/headers";
+"use client"
+
 import Section from "./Section";
 
 export default async function Home() {
-  headers()
-
-  const items = await fetch(new URL('/api/items', process.env.API_URL))
-    .then(res => res.json())
-    .catch(() => [])
-
   return (
-    <Section title="Recommended for you" items={items} />
+    <Section title="Recommended for you" />
   )
 }

c78140f6116fbeb190808b7da91ae4c508f6fa02
xlzior <wenjun.lye@gmail.com>
2023-05-29 23:36:36 +0800

    Fix redirect uri

--- lib/sgidClient.ts
+++ lib/sgidClient.ts
@@ -4,7 +4,7 @@ const sgidClient = new SgidClient({
   clientId: String(process.env.SGID_CLIENT_ID),
   clientSecret: String(process.env.SGID_CLIENT_SECRET),
   privateKey: String(process.env.SGID_PRIVATE_KEY),
-  redirectUri: 'http://localhost:3000/auth/redirect',
+  redirectUri: `${process.env.API_URL}/auth/redirect`,
 });
 
 export { sgidClient };

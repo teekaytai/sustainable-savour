975bab309332d1951ed5e1f073f27f892518bad2
xlzior <wenjun.lye@gmail.com>
2023-05-29 22:42:16 +0800

    Add empty fallback for items

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Section from "./Section";
 
 export default async function Home() {
-  const items = await fetch(new URL('/api/items', process.env.API_URL)).then(res => res.json())
+  const items = await fetch(new URL('/api/items', process.env.API_URL))
+    .then(res => res.json())
+    .catch(() => [])
 
   return (
-    <>
-      <Section title="Recommended for you" items={items} />
-    </>
+    <Section title="Recommended for you" items={items} />
   )
 }
